3072ee83e82b724d1f97e5bbcbd8546e65e0a597
Lucas Mota <lucas.o.mota@gmail.com>
2021-01-17 19:14:54 -0300

    feature(fonts):Adicionado fontes do projeto

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -7,12 +7,15 @@ export default createGlobalStyle`
   box-sizing: border-box;
 }
 body{
-  background: white;
+  background: #299CAB;
   color: #181515;
   -webkit-font-smooting: antialiased;
+  max-width: 800px;
+  margin: auto;
 }
 body, input, button{
-  font-family: 'Roboto Slab', serif;
+  font-family: 'Rubik', sans-serif;
+
   font-size: 16px;
 }
 h1, h2, h3, h4, h5, h6, strong{
